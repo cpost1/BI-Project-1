3011a0158ae8f15c4e6b8afdf2a311a0ced4c784
cpost1 <cpost1@babson.edu>
2018-03-12 20:29:59 -0400

    all tables added

--- Lush_dbcreation.sql
+++ Lush_dbcreation.sql
@@ -11,8 +11,7 @@ CREATE TABLE Sales(
 	ProductID int NOT NULL,
 	SalesTotal float NOT NULL,
 	SaleDate datetime NOT NULL,
-	OrderQuantity int NOT NULL)
-  ;
+	OrderQuantity int NOT NULL);
 
 CREATE TABLE Customers(
 	CustomerID int NOT NULL PRIMARY KEY,
@@ -33,14 +32,33 @@ CREATE TABLE Employees(
 
 
 CREATE TABLE Product(
-	Product ID
+	ProductID int NOT NULL PRIMARY KEY,
+	IngredientID int NOT NULL,
+	ProductName varchar(50) NOT NULL,
+	ProductCategory varchar(50) NOT NULL,
+	ProductNumber int NOT NULL,
+	ListPrice float NOT NULL);
+	
 
 CREATE TABLE Ingredients(
+	IngredientID int NOT NULL PRIMARY KEY,
+	IngredientsName varchar(50) NOT NULL,
+	Supplier varchar(50) NOT NULL,
+	Price float NOT NULL);
 	
 
 
 ALTER TABLE Sales ADD CONSTRAINT FK_Sales_Customer 
 FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
+
+ALTER TABLE Sales ADD CONSTRAINT FK_Sales_Emoloyee 
+FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID);
+
+ALTER TABLE Sales ADD CONSTRAINT FK_Sales_Product 
+FOREIGN KEY (ProductID) REFERENCES Product(ProductID);
+
+ALTER TABLE Product ADD CONSTRAINT FK_Product_Ingredient 
+FOREIGN KEY (IngredientID) REFERENCES Ingredients(IngredientID);
 
 GO
 
