60e73dd5b9878cf1abb982887083c35b5e7a285e
cpost1 <cpost1@babson.edu>
2018-03-12 20:19:15 -0400

    Updated

--- Lush_dbcreation.sql
+++ Lush_dbcreation.sql
@@ -5,29 +5,35 @@ USE LushCosmetics;
 GO
 
 CREATE TABLE Sales(
-  SaleID int NOT NULL PRIMARY KEY,
-  CustomerID int NOT NULL,
-  EmployeeID int NOT NULL,
-  SaleTotal float NOT NULL,
-  SaleDate datetime NOT NULL);
+	SaleID int NOT NULL PRIMARY KEY,
+	CustomerID int NOT NULL,
+	EmployeeID int NOT NULL,
+	ProductID int NOT NULL,
+	SalesTotal float NOT NULL,
+	SaleDate datetime NOT NULL,
+	OrderQuantity int NOT NULL)
+  ;
 
 CREATE TABLE Customers(
-  CustomerID int NOT NULL PRIMARY KEY,
-  FirstName varchar(50) NOT NULL,
-  LastName varchar(50)NOT NULL,
-  CustomerAddress varchar(50) NOT NULL,
-  Age tinyint NOT NULL);
+	CustomerID int NOT NULL PRIMARY KEY,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50)NOT NULL,
+	CustomerAddress varchar(50) NOT NULL,
+	PhoneNumber int NOT NULL,
+	CreditCardNumber int NOT NULL);
 
 
 CREATE TABLE Employees(
-  EmployeeID int NOT NULL PRIMARY KEY,
-  FirstName varchar(50) NOT NULL,
-  LastName varchar(50) NOT NULL,
-  Title varchar(50) NOT NULL,
-  DateHired datetime NOT NULL);
+	EmployeeID int NOT NULL PRIMARY KEY,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	Title varchar(50) NOT NULL,
+	DateHired datetime NOT NULL,
+	Age tinyint NOT NULL);
 
 
 CREATE TABLE Product(
+	Product ID
 
 CREATE TABLE Ingredients(
 	
