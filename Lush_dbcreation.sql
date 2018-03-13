e50dec57ed0a3a4b59a8dbfb1d5dac4128cfd87f
cpost1 <cpost1@babson.edu>
2018-03-12 20:13:26 -0400

    begining database creation

---
+++ Lush_dbcreation.sql
@@ -0,0 +1,40 @@
+﻿CREATE DATABASE LushCosmetics;
+GO
+
+USE LushCosmetics;
+GO
+
+CREATE TABLE Sales(
+  SaleID int NOT NULL PRIMARY KEY,
+  CustomerID int NOT NULL,
+  EmployeeID int NOT NULL,
+  SaleTotal float NOT NULL,
+  SaleDate datetime NOT NULL);
+
+CREATE TABLE Customers(
+  CustomerID int NOT NULL PRIMARY KEY,
+  FirstName varchar(50) NOT NULL,
+  LastName varchar(50)NOT NULL,
+  CustomerAddress varchar(50) NOT NULL,
+  Age tinyint NOT NULL);
+
+
+CREATE TABLE Employees(
+  EmployeeID int NOT NULL PRIMARY KEY,
+  FirstName varchar(50) NOT NULL,
+  LastName varchar(50) NOT NULL,
+  Title varchar(50) NOT NULL,
+  DateHired datetime NOT NULL);
+
+
+CREATE TABLE Product(
+
+CREATE TABLE Ingredients(
+	
+
+
+ALTER TABLE Sales ADD CONSTRAINT FK_Sales_Customer 
+FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
+
+GO
+
