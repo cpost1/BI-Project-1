19b320e76254fa5f8250d172021298c1ff81bd4a
cpost1 <cpost1@babson.edu>
2018-03-14 21:33:25 -0400

    final db creation

--- LushCosmetics_dbcreation.sql
+++ LushCosmetics_dbcreation.sql
@@ -18,8 +18,8 @@ CREATE TABLE Customers(
 	FirstName varchar(50) NOT NULL,
 	LastName varchar(50)NOT NULL,
 	CustomerAddress varchar(50) NOT NULL,
-	PhoneNumber int NOT NULL,
-	CreditCardNumber int NOT NULL);
+	PhoneNumber varchar(50) NOT NULL,
+	CreditCardNumber varchar(50) NOT NULL);
 
 
 CREATE TABLE Employees(
@@ -62,51 +62,51 @@ GO
 
 INSERT INTO Sales(SaleID, CustomerID, EmployeeID, ProductID, SalesTotal, SaleDate, OrderQuantity) values
 (1, 1, 1, 1, 32, '2016-03-04 08:43:12.000', 4),
-(2, 5, 3, 5, 240, '2017-05-12 08:43:12.000', 12), 
-(3, 1, NULL, 4, 402, '2018-01-20 08:43:12.000', 13),
-(4, 3, 8, 9, 4202, '2018-03-10 08:43:12.000', 2),
-(5, 6, NULL, 3, 124, '2018-03-10 08:43:12.000', 3),
-(6, 10, NULL, 2, 231, '2018-03-10 08:43:12.000',1),
-(7, 7, 4, 10, 134, '2018-03-10 08:43:12.000', 5),
-(8, 2, NULL, 7, 53, '2018-03-10 08:43:12.000', 2),
-(9, 8, NULL, 8, 121, '2018-03-10 08:43:12.000', 3),
-(10, 9, NULL, 6, 147, '2018-03-10 08:43:12.000', 4);
+(2, 2, 2, 2, 240, '2017-05-12 08:43:12.000', 12), 
+(3, 3, NULL, 3, 402, '2018-01-20 08:43:12.000', 13),
+(4, 4, 4, 4, 4202, '2018-03-10 08:43:12.000', 2),
+(5, 5, NULL, 5, 124, '2018-03-10 08:43:12.000', 3),
+(6, 6, NULL, 6, 231, '2018-03-10 08:43:12.000',1),
+(7, 7, 7, 7, 134, '2018-03-10 08:43:12.000', 5),
+(8, 8, NULL, 8, 53, '2018-03-10 08:43:12.000', 2),
+(9, 9, NULL, 9, 121, '2018-03-10 08:43:12.000', 3),
+(10, 10, NULL, 10, 147, '2018-03-10 08:43:12.000', 4);
 
 INSERT INTO Customers(CustomerID, FirstName, LastName, CustomerAddress, PhoneNumber, CreditCardNumber) values
-(1, 'Nicki', 'Hodgdon', 1453, 2512846098, 4485539789963790),
-(2, 'Donetta', 'Casselman', 46544, 7189579961, 4929351766116770),
-(3, 'Fletcher', 'Depaul', 44203, 2142402985, 4556210736823560),
-(4, 'Waltraud', 'Wince', 60142, 7192797978, 4024007172117200),
-(5, 'Lauri', 'Lemos', 42001, 2707793966, 4532758372114020),
-(6, 'Somer', 'Fischbach', 23834, 3392373386, 5484086800178580),
-(7, 'Ellamae', 'Shumpert', 2852, 2036693778, 5111151011342550),
-(8, 'Rodrigo', 'Collar', 23832, 2819572317, 5479094874789780),
-(9, 'Tegan', 'Lackner', 61350, 6714711369, 5272570202477050),
-(10, 'Hosea', 'Jacome', 85718, 2604232548, 5526826910034710);
+(1, 'Nicki', 'Hodgdon', '01453', '2512846098', '4485539789963790'),
+(2, 'Donetta', 'Casselman', '46544', '7189579961', '4929351766116770'),
+(3, 'Fletcher', 'Depaul', '44203', '2142402985', '4556210736823560'),
+(4, 'Waltraud', 'Wince', '60142', '7192797978', '4024007172117200'),
+(5, 'Lauri', 'Lemos', '42001', '2707793966', '4532758372114020'),
+(6, 'Somer', 'Fischbach', '23834', '3392373386', '5484086800178580'),
+(7, 'Ellamae', 'Shumpert', '02852', '2036693778', '5111151011342550'),
+(8, 'Rodrigo', 'Collar', '23832', '2819572317', '5479094874789780'),
+(9, 'Tegan', 'Lackner', '61350', '6714711369', '5272570202477050'),
+(10, 'Hosea', 'Jacome', '85718', '2604232548', '5526826910034710');
 
 INSERT INTO Employees(EmployeeID, FirstName, LastName, Title, DateHired, Age) values
 (1, 'Chris', 'Post', 'Sales Representative', '2016-03-04 08:43:12.000', 52),
-(2, 'Christain', 'Gallagher', 'Marketing', '2012-08-22 09:53:56.223', 32),
+(2, 'Christain', 'Gallagher', 'Sales Representative', '2012-08-22 09:53:56.223', 32),
 (3, 'Alexis', 'Cohen', 'IT', '2014-10-01 09:53:56.223', 24),
-(4, 'Pierre', 'Morris', 'Analyst', '2010-01-22 09:53:56.223', 42),
-(5, 'Jaylen', 'Brown','Sales Representative', '2015-05-23 09:53:56.223', 64),
+(4, 'Pierre', 'Morris', 'Sales Representative', '2010-01-22 09:53:56.223', 42),
+(5, 'Jaylen', 'Brown','Marketing', '2015-05-23 09:53:56.223', 64),
 (6, 'Jayson','Tatum', 'Marketing', '2016-08-22 09:53:56.223', 29),
-(7, 'Kelly', 'Flynn', 'Accountant', '2017-08-22 09:53:56.223', 45),
+(7, 'Kelly', 'Flynn', 'Sales Representative', '2017-08-22 09:53:56.223', 45),
 (8, 'Nicole', 'Davis', 'Human Relations', '2018-01-22 09:53:56.223', 38),
-(9, 'Brad', 'Stevens', 'Sales Representative', '2017-04-12 09:53:56.223', 42),
-(10, 'Marcus', 'Smart', 'Sales Representative', '2012-04-22 09:53:56.223', 34);
+(9, 'Brad', 'Stevens', 'Analyst', '2017-04-12 09:53:56.223', 42),
+(10, 'Marcus', 'Smart', 'Accountant', '2012-04-22 09:53:56.223', 34);
 
 INSERT INTO Product(ProductID, IngredientID, ProductName, ProductCategory, ProductNumber) values
-(1, 972, 'Dark Angels', 12, 77889900),
-(2, 543, 'Mask of Managmity',3, 23498740),
-(3, 308, 'Ocean Salt', 12, 76584937),
-(3, 100, 'Ocean Salt', 12, 76584937),
-(3, 876, 'Ocean Salt', 12, 76584937),
-(4, 972, 'Ugai Mouthwash Tab', 5, 12789320),
-(7, 100, 'Oatafix', 3, 07964210),
-(8, 456, 'Beautiful Day Shower Gel', 1, 56420891),
-(9, 456, 'Charity Pot Lotion', 2, 34097516),
-(10, 230, 'Hottie Body Bar', 3, 12095648);
+(1, 1, 'Dark Angels', 12, 700),
+(2, 2, 'Mask of Managmity',3, 234),
+(3, 3, 'Ocean Salt', 12, 765),
+(4, 4, 'Ocean Salt', 12, 765),
+(5, 5, 'Ocean Salt', 12, 765),
+(6, 6, 'Ugai Mouthwash Tab', 5, 127),
+(7, 7, 'Oatafix', 3, 07964210),
+(8, 8, 'Beautiful Day Shower Gel', 1, 564),
+(9, 9, 'Charity Pot Lotion', 2, 340),
+(10, 10, 'Hottie Body Bar', 3, 120);
 
 INSERT INTO Ingredients(IngredientID, IngredientsName, Supplier, Price) values
 (1, 'Coconut_Oil', 'CocoMart', 0.80),
