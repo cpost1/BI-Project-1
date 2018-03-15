59a4df39901c0e03a66b8182a7657bd9db1f9188
cpost1 <cpost1@babson.edu>
2018-03-14 21:43:17 -0400

    updated

---
+++ LushCosmetics_Queries.sql
@@ -0,0 +1,18 @@
+/* Lush Cosmetics: Chris Post, Alexis Cohen, Pierre Moutschen */
+USE LushCosmeticsDB; 
+
+SELECT *
+FROM Sales;
+
+SELECT *
+FROM Customers;
+
+SELECT *
+FROM Employees;
+
+SELECT *
+FROM Product;
+
+SELECT *
+FROM Ingredients;
+
