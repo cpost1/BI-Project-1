c2db82f700c82600657a6e19da3a75ae8cebd50b
cpost1 <cpost1@babson.edu>
2018-03-14 21:47:39 -0400

    outer join

--- LushCosmetics_Queries.sql
+++ LushCosmetics_Queries.sql
@@ -16,3 +16,10 @@ FROM Product;
 SELECT *
 FROM Ingredients;
 
+
+/*Finding out which customers have made purchases without using a sales representative.
+Left outer join is used to eliminate entries where EmployeeID is null in the sales table*/
+SELECT Customers.FirstName
+	,Customers.LastName
+FROM Sales left outer join Customers
+	on Sales.CustomerID = Customers.CustomerID;
